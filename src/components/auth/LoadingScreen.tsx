01971d0804a3a148d937fa42b31a84b9a31ebb0f
40Seventh <jzl158@gmail.com>
2026-01-26 23:21:19 -0500

    Move mission banner up significantly and reduce confetti to 3 seconds
    - Move banner from bottom-[99px] to bottom-32 (128px from bottom)
    - Much closer to trophy menu (at bottom-40)
    - Reduce confetti duration from 5 seconds to 3 seconds

--- src/components/auth/LoadingScreen.tsx
+++ src/components/auth/LoadingScreen.tsx
@@ -9,7 +9,7 @@ export function LoadingScreen({ onStartExploring }: LoadingScreenProps) {
 
   useEffect(() => {
     // Trigger confetti animation with bigger pieces
-    const duration = 5000; // Run for 5 seconds
+    const duration = 3000; // Run for 3 seconds
     const end = Date.now() + duration;
 
     const frame = () => {
